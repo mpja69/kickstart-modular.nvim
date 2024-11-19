723f67bced85337bf9d81a8bbde40b0dac9223aa
Magnus Andersson <mpja69@gmail.com>
2024-11-19 19:13:46 +0100

    comment

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -33,6 +33,7 @@ return {
     local dapui = require 'dapui'
     return {
       -- Basic debugging keymaps, feel free to change to your liking!
+      -- mpja69: My debug settings with OPT-ARROWs
       { '<M-d>', dap.continue, desc = 'Debug: Start/Continue' },
       { '<M-Down>', dap.step_into, desc = 'Debug: Step Into' },
       { '<M-Right>', dap.step_over, desc = 'Debug: Step Over' },
@@ -114,8 +115,6 @@ return {
     -- Install golang specific config
     require('dap-go').setup {
       delve = {
-        -- On Windows delve must be run attached or it crashes.
-        -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
         detached = vim.fn.has 'win32' == 0,
       },
     }
